d3b987c2eca6e383eb8b290c809f1b882c108bb3
Andrew <aj164astros@gmail.com>
2020-04-21 12:59:07 -0500

    work done class Farmer.py class Simulation.py

---
+++ Farmer.py
@@ -0,0 +1,29 @@
+import toolbox
+
+class Farmer(object):
+
+    def __init__(self):
+        self.__farmHands = 0
+        self.__family = 0
+        self.__money = 0
+        self.__totalPlots = 0
+
+    def hire_farmHands(self):
+        """
+        Adds a farmHand to the total amount of farmHands.
+        :return:
+        """
+        confirm = toolbox.get_boolean("Are you sure you want to hire a farm hand? : ")
+        if confirm:
+            self.__farmHands += 1
+
+    def fire_farmHands(self):
+        """
+        Removes a farmHand from the total amount.
+        :return:
+        """
+        confirm = toolbox.get_boolean("Are you sure you want to fire a farm hand? : ")
+        if confirm:
+            amount = toolbox.get_integer_between(1, self.__farmHands, "How many farm hands would you like to fire?: ")
+            self.__farmHands = self.__farmHands - amount
+
---
+++ Simulation.py
@@ -0,0 +1,11 @@
+class Simulation(object):
+
+    def __init__(self):
+        self.__plotList = []
+
+    def advnace(self):
+        """
+        This will advance the simulation forward one year.
+        :return: None
+        """
+        pass
